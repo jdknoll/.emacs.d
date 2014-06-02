91e812d5f498be1cbedcea8359f94baf819dd598
jdknoll <muddles17@gmail.com>
2014-06-02 00:45:23 -0400

    added languages and terminal fix

--- init.el
+++ init.el
@@ -9,25 +9,44 @@
 (require 'init-utils)
 (require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
 (require 'init-elpa)      ;; Machinery for installing required packages
+(require 'init-exec-path)
+
+;;----------------------------------------------------------------------------
+;; Load configs for specific features and modes
+;;----------------------------------------------------------------------------
 
 (require-package 'diminish)
+
+(require 'init-frame-hooks)
+(require 'init-xterm)
+(require 'init-themes)
 
 (require 'init-evil)
 (require 'init-graphene)
 (require 'init-saves)
-(require 'init-themes)
+(require 'init-mmm)
 (require 'init-sr-speedbar)
+
+(require 'init-sql)
+(require 'init-php)
 (require 'init-rails)
+(require 'init-ruby-mode)
 (require 'init-slime)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;;----------------------------------------------------------------------------
+;; Allow access from emacsclient
+;;----------------------------------------------------------------------------
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+;;----------------------------------------------------------------------------
+;; Variables configured via the interactive 'customize' interface
+;;----------------------------------------------------------------------------
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+(require 'init-locales)
+
+(provide 'init)
