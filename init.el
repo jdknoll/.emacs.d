06b5ada6ac6c5b3f9f55ea91b24ad8f709004c77
Josiah Knoll <josiahknoll@gmail.com>
2014-06-05 12:23:20 -0400

    added more functionality from purcell's emacs

--- init.el
+++ init.el
@@ -1,5 +1,14 @@
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+;;; This file bootstraps the configuration, which is divided into
+;;; a number of other files.
 
+(let ((minver 23))
+  (unless (>= emacs-major-version minver)
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'init-benchmarking) ;; Measure startup time
+
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
 
 ;;----------------------------------------------------------------------------
@@ -15,22 +24,32 @@
 ;; Load configs for specific features and modes
 ;;----------------------------------------------------------------------------
 
+(require-package 'wgrep)
 (require-package 'diminish)
+(require-package 'scratch)
 
 (require 'init-frame-hooks)
 (require 'init-xterm)
 (require 'init-themes)
+(require 'init-uniquify)
 
 (require 'init-evil)
 (require 'init-graphene)
 (require 'init-saves)
-(require 'init-mmm)
 (require 'init-sr-speedbar)
+
+(require 'init-auto-complete)
+(require 'init-mmm)
+
+(require 'init-editing-utils)
 
 (require 'init-sql)
 (require 'init-php)
-(require 'init-rails)
 (require 'init-ruby-mode)
+(require 'init-rails)
+
+(require 'init-paredit)
+(require 'init-lisp)
 (require 'init-slime)
 
 ;;----------------------------------------------------------------------------
